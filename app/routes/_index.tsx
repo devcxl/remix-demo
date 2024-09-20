8d7e8d1504c5736550ae2317fb2c86bf9c897437
devcxl <64475363+devcxl@users.noreply.github.com>
2024-09-21 02:13:47 +0800

    install commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <p id="index-page">
+      This is a demo for Remix.
+      <br />
+      Check out <a href="http://remix.org.cn">the docs at remix.run</a>.
+    </p>
+  );
+}
